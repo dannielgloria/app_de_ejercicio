1a86816b1b4626021b8bd5091c12bfbfb5b777d5
Daniel Gloria <danniel.gloria@gmail.com>
2020-07-01 20:21:13 -0500

    git push origin master

---
+++ lib/app/app.dart
@@ -0,0 +1,14 @@
+import 'package:app_de_ejercicio/screens/SplashScreen.dart';
+import 'package:flutter/material.dart';
+
+class AppExercise extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      home: SplashScreen(),
+    );
+  }
+}
